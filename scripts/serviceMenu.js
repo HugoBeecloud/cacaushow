e914a4150e505b4557b0d973c761ab0c7abfdeab
manzolimatheusbee <118744003+manzolimatheusbee@users.noreply.github.com>
2024-04-08 11:04:15 -0300

    Update serviceMenu.js

--- scripts/serviceMenu.js
+++ scripts/serviceMenu.js
@@ -194,7 +194,7 @@ function hideBaseElements() {
 }
 
 function runCustomChat() {
-  console.log("👋 CustomChat Running...");
+  console.log("👋🗨 CustomChat Running...");
   hideBaseElements();
   buildChatMenu();
 }
